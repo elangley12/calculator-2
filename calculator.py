2513460b3ba0a8f8c74b9cbe1ddf0da775d614fb
Emily <emily.j.langley@gmail.com>
2024-02-28 17:25:36 -0800

    Added token checks based on length and updated print statements

--- calculator.py
+++ calculator.py
@@ -5,39 +5,62 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 while True:
-    user_input = input("Enter your equation: ").lower()
-    user_input = user_input.split(" ")
+    user_input = input("Enter your equation > ")
+    tokens = user_input.split(" ")
 
-    if user_input[0] == 'q':
+    if 'q' in tokens:
         print("You will exit.")
         break
+    elif len(tokens) < 2: 
+        print('Not enough inputs.')
+        continue
+        
+    operator = tokens[0]
+    num1 = tokens[1]
 
-    elif user_input[0] == '+':
-        print(add(float(user_input[1]), float(user_input[2])))
+    if len(tokens) < 3: 
+        num2 = '0'
+
+    else: 
+        num2 = tokens[2]
     
-    elif user_input[0] == '-':
-        print(subtract(user_input[1], user_input[2]))
+    if len(tokens) > 3: 
+        num3 = tokens[3]
+
+    result = None
+
+    if not num1.isdigit() or not num2.isdigit(): 
+        print("Those aren't numbers!")
+        continue
+
+    elif operator == '+':
+        result = add(float(num1), float(num2))
     
-    elif user_input[0] == '*':
-        print(multiply(user_input[1], user_input[2]))
+    elif operator == '-':
+        result = subtract(float(num1), float(num2))
+    
+    elif operator == '*':
+        result = multiply(float(num1), float(num2))
 
-    elif user_input[0] == '/':
-        divide(user_input[1], user_input[2])
+    elif operator == '/':
+        result = divide(float(num1), float(num2))
 
-    elif user_input[0] == 'square':
-        square(user_input[1], user_input[2])
+    elif operator == 'square':
+        result = square(float(num1), float(num2))
 
-    elif user_input[0] == 'cube':
-        cube(user_input[1], user_input[2])
+    elif operator == 'cube':
+        result = cube(float(num1), float(num2))
 
-    elif user_input[0] == 'pow':
-        power(user_input[1], user_input[2])
+    elif operator == 'pow':
+        result = power(float(num1), float(num2))
 
-    elif user_input[0] == 'mod':
-        mod(user_input[1], user_input[2])
+    elif operator == 'mod':
+        result = mod(float(num1), float(num2))
 
     else:
         print("Please type that again.")
+
+    print(result)
 
     
 
